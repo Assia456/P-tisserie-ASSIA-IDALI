c36ef9a20f32b180e43649cc3d1ab21fc88f391e
Assia456 <assiaidali456@gmail.com>
2025-05-21 17:23:05 +0200

    jSHDLOU

---
+++ app.js
@@ -0,0 +1,7 @@
+   fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        //ici et uniquement ici j'ai acces a mon tablea de données
+        afficherArticles(data)
+
+    });
